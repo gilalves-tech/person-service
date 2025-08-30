f3d1bc474cf28a944d12b0fd1a7a1b3cdd90496d
Gil Alves <ggvr.alves@gmail.com>
2025-08-31 00:45:27 +0200

    Base Resource

---
+++ lib/resources/base-resource.ts
@@ -0,0 +1,8 @@
+import { Construct } from "constructs";
+import { IConfig } from "../config/config.interface";
+
+export abstract class BaseResource extends Construct {
+	constructor(scope: Construct, id: string, protected readonly config: IConfig) {
+		super(scope, id);
+	}
+}
