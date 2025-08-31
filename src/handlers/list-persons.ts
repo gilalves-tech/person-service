7b8f46c7a4a84a1cb62ebfb1afe5a700ddea7c29
Gil Alves <ggvr.alves@gmail.com>
2025-08-31 11:29:01 +0200

    Uncomment list-persons

--- src/handlers/list-persons.ts
+++ src/handlers/list-persons.ts
@@ -1,16 +1,15 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
-// import { DynamoDbClient } from "../infrastructure/dynamo-db-client";
+import { DynamoDbClient } from "../infrastructure/dynamo-db-client";
 
-// const dynamoDbClient = DynamoDbClient.getInstance();
+const dynamoDbClient = DynamoDbClient.getInstance();
 
 export const handler: APIGatewayProxyHandlerV2 = async () => {
 	try {
-		// const persons = await dynamoDbClient.listPersons();
+		const persons = await dynamoDbClient.listPersons();
 
 		return {
 			statusCode: 200,
-			// body: JSON.stringify(persons),
-			body: JSON.stringify([]),
+			body: JSON.stringify(persons),
 		};
 	} catch (err) {
 		console.error("Error listing persons:", err);
