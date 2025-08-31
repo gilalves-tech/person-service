ab0e8467810b7cb7844aeb7dccf006c5ead17ce6
Gil Alves <ggvr.alves@gmail.com>
2025-08-31 09:49:07 +0200

    Implement Sns Client singleton class

---
+++ src/infrastructure/sns-client.ts
@@ -0,0 +1,45 @@
+import { SNSClient as AwsSNSClient, PublishCommand } from "@aws-sdk/client-sns";
+import { Person } from "src/interfaces/person.interface";
+import { EventType } from "src/enums/event-type.enum";
+import { accessEnv } from "src/utils/accessEnv";
+
+export class SnsClient {
+	private static instance: SnsClient;
+	private client: AwsSNSClient;
+	private topicArn: string;
+
+	private constructor() {
+		const region = accessEnv("REGION");
+		this.topicArn = accessEnv("SNS_TOPIC_ARN");
+
+		this.client = new AwsSNSClient({ region });
+	}
+
+	public static getInstance(): SnsClient {
+		if (!SnsClient.instance) {
+			SnsClient.instance = new SnsClient();
+		}
+		return SnsClient.instance;
+	}
+
+	async publish(eventType: EventType, message: Person): Promise<void> {
+		const params = {
+			TopicArn: this.topicArn,
+			Message: JSON.stringify(message),
+			MessageAttributes: {
+				eventType: {
+					DataType: "String",
+					StringValue: eventType.valueOf(),
+				},
+			},
+		};
+
+		try {
+			await this.client.send(new PublishCommand(params));
+			console.log(`✅ Event published to ${this.topicArn}`, params);
+		} catch (error) {
+			console.error("❌ Failed to publish event", error);
+			throw error;
+		}
+	}
+}
